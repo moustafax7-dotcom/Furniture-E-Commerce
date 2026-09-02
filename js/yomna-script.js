d362699d047ba494d3e03e88388eedc3ee17c7eb
ahmedmohmed251 <ahmadmohmed251@gmail.com>
2026-09-02 21:44:52 +0300

    index

--- js/yomna-script.js
+++ js/yomna-script.js
@@ -38,3 +38,16 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+fetch("navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    });
+
+
+fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+    });
