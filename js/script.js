646d45d55c9bd514acde3a7a91f6ded86ccffdc1
ahmedmohmed251 <ahmadmohmed251@gmail.com>
2026-09-02 17:37:43 +0300

    index.html & Order-success.html

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// <----------------------------------index------------------------------------>
+let categories = document.querySelectorAll(".category-menu li");
+let products = document.querySelectorAll(".product-item");
+
+for (let i = 0; i < categories.length; i++) {
+    categories[i].onclick = function () {
+        let filter = categories[i].getAttribute("data-filter");
+
+        for (let j = 0; j < categories.length; j++) {
+
+            categories[j].classList.remove("active-category");
+        }
+
+        categories[i].classList.add("active-category");
+
+        for (let j = 0; j < products.length; j++) {
+            let productCategory = products[j].getAttribute("data-category");
+
+            if (filter == "all" || filter == productCategory) {
+                products[j].style.display = "block";
+            }
+
+            else {
+                products[j].style.display = "none";
+            }
+        }
+    };
+}
+// <----------------------------------index------------------------------------>
